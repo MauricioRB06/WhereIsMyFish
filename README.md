0ec8739813c457123de463558a327a258e6070e6
Mauricio Rodriguez Becerra <mrodriguezbec@unal.edu.co>
2022-04-29 02:04:44 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# WhereIsMyFish
+ WhereIsMyFish is a videogame developed with Unreal Engine 5, where we control Tuna, an intrepid and gluttonous kitten that is always looking for a good fish to eat, we must help him solve the different mazes and puzzles to reach his favorite food, do you think you can help him?
---
+++ Source/WhereIsMyFish/WhereIsMyFishGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WhereIsMyFishGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WHEREISMYFISH_API AWhereIsMyFishGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
